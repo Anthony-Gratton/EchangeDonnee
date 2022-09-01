9a3718d4a516e8e7a1a4d6d3135d8ef4dcb1402f
Anthony-Gratton <2060246@cstj.qc.ca>
2022-09-01 15:01:19 -0400

    cours semaine 2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { response } from "express";
+import dayjs from "dayjs";
 
 const app = express()
 
@@ -8,5 +9,50 @@ app.get("/premiere", (req, res) => {
     res.set("content-type", "text/plain")
     res.send("première route avec express")
 })
+
+app.get("/date", (req, res) => {
+    res.status(200)
+    res.set("content-type", "text/plain")
+    res.send(dayjs().format())
+})
+
+app.get("/maths/:operator", (req, res) => {
+    const a = req.query.a;
+    const b = req.query.b;
+
+    let result;
+    const operation = req.params.operator
+
+    switch (operation) {
+        case "somme":
+            result = Number(a) + Number(b);
+            break;
+
+        case "quotient":
+            result = Number(a) / Number(b);
+            break;
+
+        case "difference":
+            result = Number(a) - Number(b);
+            break;
+
+
+        case "produit":
+            result = Number(a) * Number(b);
+            break;
+
+        case "reste":
+            result = Number(b) % Number(a);
+            break;
+
+        default:
+            break;
+    }
+
+    res.status(200);
+    res.set("content-type", "text/plain");
+    res.send(String(result));
+})
+
 
 export default app;
