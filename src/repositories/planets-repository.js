c1236ee38c1d9dfeb8029c409706b17476866245
Anthony-Gratton <2060246@cstj.qc.ca>
2022-09-20 09:35:02 -0400

    post things

--- src/repositories/planets-repository.js
+++ src/repositories/planets-repository.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import planetModel from "../models/planet-model.js";
 import Planet from "../models/planet-model.js"
 
 class PlanetsRepository {
@@ -5,6 +7,26 @@ class PlanetsRepository {
     retrieveAll() {
         return Planet.find(); //select * from planets
     }
+
+    retrieveOne(idPlanet) {
+        return Planet.findById(idPlanet); //Select *from planetes Where idPlanet = idPlanet
+    }
+
+    create(planet) {
+        return Planet.create(planet); //INSERT () INTO planets
+    }
+
+
+    transform(planet) {
+
+        planet.discoveryDate = dayjs(planet.discoveryDate).format("YYYY-MM-DD");
+        delete planet.createdAt;
+        delete planet.updatedAt;
+        delete planet.__v;
+
+        return planet;
+    }
+
 }
 
 export default new PlanetsRepository();
--- src/routes/planets-routes.js
+++ src/routes/planets-routes.js
@@ -32,53 +32,41 @@ class PlanetsRoutes {
     async getAll(req, res, next) {
         try {
             const planets = await planetsRepository.retrieveAll();
-            console.log(planets)
             res.status(200).json(planets);
         } catch (err) {
             return next(err);
         }
     }
 
-    getOne(req, res, next) {
+    async getOne(req, res, next) {
+        try {
+            let planet = await planetsRepository.retrieveOne(req.params.idPlanet);
 
-        // const idPlanet = req.params.idPlanet;
-        // for (let planet of PLANETS) {
-        //     if (planet.id === idPlanet) {
-        //         res.status(200);
-        //         res.json(planet);
-        //         break;
-        //     }
-        // }
-        // res.status(404);
-        // res.end();
-        const idPlanet = parseInt(req.params.idPlanet, 10);
-        const planet = PLANETS.filter(p => p.id === idPlanet);
-        if (planet.length > 0) {
-            res.status(200);
-            res.json(planet[0]);
+            if (!planet) {
+                return next(HttpError.NotFound(`La planète avec l'identifiant: ${req.params.idPlanet} n'existe pas`))
+            }
+            planet = planet.toObject({ getters: false, virtuals: false });
+            planet = planetsRepository.transform(planet)
+            res.status(200).json(planet);
+        } catch (err) {
+            return next(err);
         }
-        else {
-            return next(HttpError.NotFound("404"));
-        }
-
 
     }
 
 
-    post(req, res, next) {
-        const newPlanet = req.body;
-        if (newPlanet) {
-            const index = PLANETS.findIndex(p => p.id === req.body.id);
-            if (index === - 1) {
-                PLANETS.push(newPlanet);
-                res.status(201).json(newPlanet);
-            }
-            else {
-                return next(HttpError.Conflict(`Une planète avec l'identifiant ${req.body.id}`));
-            }
-        }
-        else {
-            return next(HttpError.BadRequest("Aucune information transmise"));
+    async post(req, res, next) {
+        try {
+
+            let newPlanet = await planetsRepository.create(req.body);
+
+            newPlanet = newPlanet.toObject({ getters: false, virtuals: false });
+            newPlanet = planetsRepository.transform(newPlanet);
+
+            res.status(201).json(newPlanet);
+
+        } catch (err) {
+            return next(err);
         }
     }
 
